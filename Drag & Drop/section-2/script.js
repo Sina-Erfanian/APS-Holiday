59626db5827de77627de8d62d7894d940b9e9feb
Sina-Erfanian <92786381+Sina-Erfanian@users.noreply.github.com>
2022-03-28 14:24:41 +0430

    add

--- Drag & Drop/section-2/script.js
+++ Drag & Drop/section-2/script.js
@@ -15,14 +15,28 @@ const richestPeople = [
 ];
 
 
-const listItem = []
+const listItems = []
 
 let dragStartIndex;
 
 createList()
 
 function createList() {
-    [...richestPeople].forEach((person,index)=> {
-        const listItem = document.crea
+    [...richestPeople]
+    .map(ele => ({value : ele , sort : Math.random()}))
+    .sort((a,b) => a.sort - b.sort)
+    .map(ele => ele.value)
+    .forEach((person,index)=> {
+        const listItem = document.createElement("li")
+        listItem.setAttribute("data-index",index)
+        listItem.innerHTML = `
+        <span class="number">${index + 1}</span>
+        <div class="draggable" draggable="true">
+          <p class="person-name">${person}</p>
+          <i class="fas fa-grip-lines"></i>
+        </div>
+        `;
+        listItems.push(listItem);
+        draggable_list.appendChild(listItem);
     })
 }
