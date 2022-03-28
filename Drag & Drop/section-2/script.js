9d585590ac7329fb7d5790b06e02108a879aff21
Sina-Erfanian <92786381+Sina-Erfanian@users.noreply.github.com>
2022-03-28 13:42:54 +0430

    add

---
+++ Drag & Drop/section-2/script.js
@@ -0,0 +1,28 @@
+const draggable_list = document.getElementById("draggable-list");
+const check = document.getElementById("check");
+
+const richestPeople = [
+  "Elon Musk",
+  "Jeff Bezos",
+  "Bernard Arnault",
+  "Bill Gates",
+  "Larry Page",
+  "Warren Buffett",
+  "Sergey Brin",
+  "Steve Ballmer",
+  "Larry Ellison",
+  "Mukesh Ambani",
+];
+
+
+const listItem = []
+
+let dragStartIndex;
+
+createList()
+
+function createList() {
+    [...richestPeople].forEach((person,index)=> {
+        const listItem = document.crea
+    })
+}
